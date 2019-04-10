526ae4d74b352258cbfa055dc92a791a49e4dc1e
Israfel Sskaara <sskaaracometh@gmail.com>
2019-04-10 10:34:00 +0100

    Text connector for PrizeModel and extension methods

---
+++ TournamentTracker/TrackerLib/DataAccess/TextConnectorProcessor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.Models;
+
+namespace TrackerLibrary.DataAccess.TextHelper
+{
+    public static class TextConnectorProcessor
+    {
+        /// <summary>
+        /// Gets files name and returns full path to textfile, 
+        /// "this" used to make an extension method 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static string FullFilePath(this string fileName)
+        {
+        //Concatenates strings
+        //slash escapes string, second is inclusive of the string
+        //Gets address of filePath then gets the filename which was inputted e.g: 
+        //C:\TournamentHandler\TournamentTracker\TextFiles\fileName
+            return $"{ConfigurationManager.AppSettings["filePath"]}\\{ fileName } ";
+        }
+
+        /// <summary>
+        /// Takes full file path and returns back list of strings representing all 
+        /// lines in the file, first line is the first entry etc.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static List<string> LoadFile(this string file)
+        {
+            if(!File.Exists(file))
+            {
+                return new List<string>();
+            }
+
+            return File.ReadAllLines(file).ToList();
+        }
+
+        /// <summary>
+        /// Builds model based on text file
+        /// </summary>
+        /// <param name="lines"></param>
+        /// <returns></returns>
+        public static List<PrizeModel> ConvertToPrizeModels(this List<string> lines)
+        {
+            List<PrizeModel> output = new List<PrizeModel>();
+
+            foreach(string line in lines)
+            {
+                string[] cols = line.Split(','); // each line seperated by a ,
+
+                PrizeModel p = new PrizeModel();
+                p.Id = int.Parse(cols[0]); //first line read in 
+                p.PlaceNumber = int.Parse(cols[1]);
+                p.PlaceName = cols[2];
+                p.PrizeAmount = decimal.Parse(cols[3]);
+                p.PrizePercentage = double.Parse(cols[4]);
+                output.Add(p);
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Takes models and file names convert it to string list
+        /// Convert filename to file path
+        /// Write all lines from the full file path, providing the lines to write
+        /// </summary>
+        /// <param name="models"></param>
+        /// <param name="fileName"></param>
+        public static void SaveToPrizeFile(this List<PrizeModel> models, string fileName)
+        {
+            List<string> lines = new List<string>();
+            
+            foreach(PrizeModel p in models)
+            {
+                lines.Add($"{p.Id},{p.PlaceNumber},{p.PlaceName},{p.PrizeAmount},{p.PrizePercentage}");
+            }
+
+            File.WriteAllLines(fileName.FullFilePath(), lines);
+        }
+    }
+}
--- TournamentTracker/TrackerLib/DataAccess/TextFileConnector.cs
+++ TournamentTracker/TrackerLib/DataAccess/TextFileConnector.cs
@@ -4,20 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelper;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextFileConnector : IDataConnection
     {
-        //TODO: Make the TFC save to the textfile
+        private const string PrizesFile = "PrizeModels.csv";
+
         /// <summary>
         /// Saves a new prize to the database
+        ///For each model, store them in their own textfile
+        ///Load all prizes, read highest id+1 for new ID 
         /// </summary>
         /// <param name="model">The prize information</param>
         /// <returns>the prize information including the unique identifier</returns>
         public PrizeModel CratePrize(PrizeModel model)
         {
-            model.Id = 1;
+            //Load Textfile
+            //Convert the text to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            //Find the ID
+            int currentId = 1;
+
+            if(prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1; //Takes list and orders by decending id property,
+            }
+
+            model.Id = currentId;
+
+            //Add the new record with the new ID
+            prizes.Add(model);
+
+            //Convert the prizes to list<string>
+            //Save the list<string> to the textfile
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
